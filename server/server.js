e6193104312f4dbf2404dd2ce68a0dc8e4df60e8
Grant <gwensignak@gmail.com>
2019-08-13 11:23:11 -0600

    set up file names

--- server/server.js
+++ server/server.js
@@ -7,6 +7,29 @@ const massive = require('massive')
 const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
 const PORT = SERVER_PORT || 5555
 
+// middleWare
 app.use(express.json())
 
-app.listen(PORT, () => console.log(`REVENGE OF THE PORT ${PORT}`))
+// Sessions
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 5
+    }
+}))
+
+// End Points Auth
+
+// End points for Cards
+
+// End points for Decks
+
+
+// Massive Set up
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    app.listen(PORT, () => console.log(`REVENGE OF THE PORT ${PORT}`))
+})
+
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom';
+
+export default (
+    <Switch>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+    </Switch>
+)
